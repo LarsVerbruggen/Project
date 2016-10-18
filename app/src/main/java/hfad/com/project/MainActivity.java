1ef4800c93d9a0612e6525adf8d232f35b010ad2
Your Name <you@example.com>
2016-10-18 11:43:41 +0200

    mega fgt

--- app/src/main/java/hfad/com/project/MainActivity.java
+++ app/src/main/java/hfad/com/project/MainActivity.java
@@ -1,8 +1,11 @@
 package hfad.com.project;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void OnTap(View view){
         AmountOfPoints = AmountOfPoints + 1;
-
+        ImageButton pepe = (ImageButton) findViewById(R.id.pepe);
     }
 }
 
-public void onClickChangeButton(View view){
-
-
-}
