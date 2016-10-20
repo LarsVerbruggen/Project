8aea55dac77c5c27d709f0873cc6f1793e5780a0
Lars Verbruggen <Lars Verbruggen>
2016-10-20 12:51:20 +0200

    added string

--- app/src/main/java/hfad/com/project/MainActivity.java
+++ app/src/main/java/hfad/com/project/MainActivity.java
@@ -3,6 +3,7 @@ package hfad.com.project;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     private int AmountOfPoints = 0;
@@ -25,9 +26,15 @@ public class MainActivity extends AppCompatActivity {
         ImageButton pepe = (ImageButton) findViewById(R.id.pepe);
         pepe.setImageResource(R.drawable.pepe2);
 
-         AmountOfPoints.toString();
-        String punten = (TextView)findViewById(R.id.DPS);
+       // AmountOfPoints.toString();
+       // String punten = (TextView)findViewById(R.id.DPS);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
 
